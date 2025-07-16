61e70600413c95924d766d72575d6b1607d0720b
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-16 06:14:08 -0300

    Update
    Create index.html

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>hello flaskR</h1>"
+    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
