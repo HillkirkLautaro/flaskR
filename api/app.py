e50c35d40bce13eb4f6009414acff507fac0c6fd
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-16 05:33:32 -0300

    Update
    Le doy una ruta temporal para q no de error favicon.ico .

--- api/app.py
+++ api/app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "<h1>hello flaskR</h1>"
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico')
