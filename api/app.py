16f7458777eeeafd57ce9fc2b0ea25c897fa2826
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-16 05:18:20 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -5,9 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "<h1>hello flaskR</h1>"
-
-# Esto es necesario para Vercel
-handler = app
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
