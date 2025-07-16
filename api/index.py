a40cb723f6df43f3c0a21a50f7f5109f55d83ace
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-16 03:54:55 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 # Asegurarse de que el directorio del proyecto esté en el path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 
 try:
     from app import app, handler
