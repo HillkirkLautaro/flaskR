24506b5ea779772383042c9cd1be38e105181fcd
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-18 05:40:47 -0300

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -12,8 +12,8 @@ def about():
     return render_template('about.html', titulo='Acerca de', contenido="Esta es una demo con Flask y Supabase en Vercel.")
 
 
-@main_bp.route('/about')
-def about():
+@main_bp.route('/encuesta')
+def encuesta():
     return render_template('encuesta.html', titulo='Encuesta', contenido="Por favor, completa la encuesta para ayudarnos a mejorar.")
 
 # Manejo de errores personalizado
