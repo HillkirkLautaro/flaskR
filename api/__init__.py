4a3fdcb10d14971869e79a3378b86f8cfb20493d
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-19 20:37:16 -0300

    Update

--- api/__init__.py
+++ api/__init__.py
@@ -3,16 +3,12 @@ from flask import Flask
 
 def create_app():
     # Obtener la ruta al directorio actual
+    static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    # Definir la ruta al directorio de plantillas
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    
-    # Verificar si estamos en producción (Vercel)
-    if 'VERCEL' in os.environ:
-        # En producción, las plantillas están en el mismo directorio
-        template_dir = os.path.join(current_dir, 'template')
-    else:
-        # En desarrollo, usar la ruta relativa estándar
-        template_dir = os.path.join(current_dir, 'template')
-    
+    # Definir la ruta al directorio de plantillas
+    template_dir = os.path.join(current_dir, 'template')
+
     # Crear la aplicación Flask
     app = Flask(__name__, 
                template_folder=template_dir,
--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+
 from api import create_app
 
+# Entry point para Flask/Vercel
 app = create_app()
