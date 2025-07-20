5b4edffa2b4bf14788becd9523f91da4e4b701ca
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-19 21:25:49 -0300

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,10 @@ import os
 from flask import Flask
 
 def create_app():
-    # Definir la ruta al directorio de plantillas
+
+    # Definir la ruta al directorio de plantillas y estáticos
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    # Definir la ruta al directorio de plantillas
-    template_dir = os.path.join(current_dir, 'template')
+    template_dir = os.path.join(current_dir, 'templates')
     static_dir = os.path.join(current_dir, 'static')
 
     # Crear la aplicación Flask
