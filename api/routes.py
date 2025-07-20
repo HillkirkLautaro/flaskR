ebb531f9ded636ea7145ded75c56d29825457c09
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-19 22:02:50 -0300

    Update
    Se creo una tabla encuesta y se integro con el formulario de encuesta.html

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template, abort, request
+from supabase import create_client, Client
+import os
+from datetime import datetime
 
 # Crear el blueprint con el prefijo de ruta
 main_bp = Blueprint('main', __name__)
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @main_bp.route('/')
 def index():
@@ -14,12 +21,17 @@ def about():
 
 @main_bp.route('/encuesta', methods=['GET', 'POST'])
 def encuesta():
+    mensaje = ""
     if request.method == 'POST':
-        # Procesar datos del formulario aquí
         nombre = request.form.get('nombre')
         email = request.form.get('email')
         opinion = request.form.get('opinion')
-        # Puedes guardar, mostrar o procesar estos datos
+        fecha = datetime.now().isoformat()
+        data = supabase.table('encuestas').insert({
+            'name': nombre,
+            'email': email,
+            'opinion': opinion,
+            'date_time': fecha
+        }).execute()
         mensaje = "¡Gracias por tu opinión, {}!".format(nombre)
-        return render_template('encuesta.html', titulo='Encuesta', contenido=mensaje)
-    return render_template('encuesta.html', titulo='Encuesta', contenido="Por favor, completa la encuesta para ayudarnos a mejorar.")
+    return render_template('encuesta.html', titulo='Encuesta', contenido=mensaje)
